4aa949b2a5967725fe0502183fbcc61115f846c4
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-11-07 12:11:51 +0530

    UPDATE: log updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -36,6 +36,8 @@ const saveUser = async (req, res) => {
         numbers: true,
       });
 
+      console.log(password);
+
       const salt = await bcrypt.genSalt();
       const hashedPassword = await bcrypt.hash(password, salt);
 
