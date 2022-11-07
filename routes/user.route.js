657a18a8dfecbf0794b0d9af8356c441ede0b4d9
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-11-07 12:23:32 +0530

    UPDATE: auth changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { saveUser, getUsers, loginUser, deleteUser, getUser } = require('../controllers/user.controller');
 const { verifyAdminAuth } = require('../middleware/auth.admin.middleware');
 
-router.get('/', getUsers);
+router.get('/', verifyAdminAuth, getUsers);
 router.post('/register', verifyAdminAuth, saveUser);
 router.post('/login', loginUser);
 router.get('/:id', verifyAdminAuth, getUser);
